2e698aff888ed8abe60e1ef3ec1a10aa4d6a1672
Jose Arturo <arturotoll5@gmail.com>
2022-02-02 15:52:48 -0500

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script context="module">
+  export async function load({ session }) {
+    if (!session.current_user) return { status: 303, redirect: "/login" };
+    return { props: null };
+  }
+</script>
+
+<script>
+  import { session } from "$app/stores";
+  let current_user = $session.current_user;
+</script>
+
+<h1>Bienvenido {current_user.name}</h1>
---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter(),
+
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    methodOverride: {
+      allowed: ['PUT', 'PATCH', 'DELETE']
+    }
+  }
+};
+
+export default config;
